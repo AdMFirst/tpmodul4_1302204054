d4776f6296ad5630282c0eec84cd86122c862729
Aditya MP <211wadityaw112@gmail.com>
2022-03-16 10:37:51 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul4_1302204054/Program.cs
+++ tpmodul4_1302204054/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace com.kpl.tp.modul4.halo
+{
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(ref T x)
+        {
+            Console.WriteLine("Halo user "+x);
+        }
+    }
+
+    public class main
+    {
+        public static void Main()
+        {
+            String nama = "Aditya Mardi Pratama";
+            HaloGeneric h = new HaloGeneric();
+            h.SapaUser(ref nama);
+        }
+    }
+}
